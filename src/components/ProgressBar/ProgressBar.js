513b3e70853648ad1400ef5c2acd7d0ddac5024b
Joel Harrison <Joel.Harrison@netscout.com>
2023-02-24 14:33:07 -0800

    Progress Bar Component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,62 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const SIZES = {
+  small: {
+    "--borderRadiusOutside": "4px",
+    "--borderRadiusInside": "4px",
+    "--height": "8px",
+  },
+  medium: {
+    "--borderRadiusOutside": "4px",
+    "--borderRadiusInside": "4px",
+    "--height": "12px",
+  },
+  large: {
+    "--borderRadiusOutside": "8px",
+    "--borderRadiusInside": "4px",
+    "--height": "24px",
+    "--padding": "4px",
+  }
 };
+
+const ProgressBar = ({ value, size }) => {
+
+  const styles = SIZES[size];
+  let radiusInner = 0;
+  if (size === 'large') {
+    radiusInner = Math.max(0, parseInt(styles["--borderRadiusOutside"]) - (366 - (value / 100) * 362));
+  } else {
+    radiusInner = Math.max(0, parseInt(styles["--borderRadiusOutside"]) - (370 - (value / 100) * 370));
+  }
+  styles["--borderRadiusInsideRight"] = radiusInner + "px";
+
+  return (
+    <ProgressWrapper aria-valuenow={value} max={100} value={value} style={styles}>Progress Bar</ProgressWrapper>
+  )
+};
+
+const ProgressWrapper = styled.progress`
+
+  &[value] {
+    -webkit-appearance: none;
+
+    width: 370px;
+    height: var(--height);
+    border-radius: var(--borderRadiusOutside);
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+
+    ::-webkit-progress-bar {
+      background-color: ${COLORS.transparentGray15};
+      border-radius: var(--borderRadiusOutside);
+      padding: var(--padding);
+    };
+    ::-webkit-progress-value {
+      background-color: ${COLORS.primary};
+      border-radius: var(--borderRadiusInside) var(--borderRadiusInsideRight) var(--borderRadiusInsideRight) var(--borderRadiusInside);
+    };
+  }
+`
+
 
 export default ProgressBar;
